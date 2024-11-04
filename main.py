2494379caac1f2d795180eb6f6b1513f89d2bc1b
and1801 <72920393+and1801@users.noreply.github.com>
2024-11-04 18:54:00 +0800

    Revert "new code 1"
    This reverts commit c7274c5654192c6d7f3b66dd5b11e8196ca59be9.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-a = 5
+
+print('я конфликтую')
+
+for i in range(8):
+    print('hy')
+a, b = 9, 8
+
+if a > b:
+    print ('a больше чем b')
+
+elif a < b:
+    print ('b больше')
+else:
+    print('a = b')
+
